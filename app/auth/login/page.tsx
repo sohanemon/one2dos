190cd93421f8b20b359848f2a0bb9bc24c48f46f
sohanemon <m.sohanemon@gmail.com>
2022-12-28 12:42:04 +0600

    analytics bugs fixed

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 import { Center } from "@chakra-ui/react";
 import HookForm from "../../../components/form/login-form";
-import { useFirebaseAuth } from "../../../contexts/auth-provider";
-import { auth } from "../../../type";
 
 export default function Page() {
   return (
--- firebase/app.ts
+++ firebase/app.ts
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_apiKey,
@@ -12,4 +11,3 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
