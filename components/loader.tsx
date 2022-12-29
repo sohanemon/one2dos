2966e06cbcff9bf3290cad0df07ec7a459194b95
sohanemon <m.sohanemon@gmail.com>
2022-12-29 10:43:59 +0600

    watch loader added

--- components/loader.tsx
+++ components/loader.tsx
@@ -1,6 +1,19 @@
 "use client";
 import { Center } from "@chakra-ui/react";
+import { Watch } from "react-loader-spinner";
 
 export default function Loader() {
-  return <Center>Loading ...</Center>;
+  return (
+    <Center>
+      <Watch
+        wrapperStyle={{ marginTop: "50px" }}
+        height='50'
+        width='50'
+        radius='48'
+        color='#ed64a6'
+        ariaLabel='watch-loading'
+        visible={true}
+      />
+    </Center>
+  );
 }
